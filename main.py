549ed4aef83a74f382a0d0f6d0d7528f7d40b958
Vazgen005 <norvardyanvazgen@gmail.com>
2022-02-19 04:01:49 +0300

    init

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from pwinput import pwinput
 import steam.webauth as wa
 import json
 import os
+import re
 
 
 if not os.path.exists('steam_cfg.txt'):
@@ -26,23 +27,27 @@ except wa.TwoFactorCodeRequired:
 session = user.session.cookies.get_dict()
 
 
-def ActivateKey(s_key: str):
-    r = user.session.post('https://store.steampowered.com/account/ajaxregisterkey/',
-                          data={
-                              'product_key': s_key,
-                              'sessionid': session["sessionid"]},
-                          headers={
-                              'Cookie': f'sessionid={session["sessionid"]}; birthtime={session["birthtime"]}; lastagecheckage=1-0-1979; Steam_Language=russian; steamCountry={session["steamCountry"]}; steamLoginSecure={session["steamLoginSecure"]}'
-                          })
-    blob = json.loads(r.text)
-    if blob["success"] == 1:
-        for item in blob["purchase_receipt_info"]["line_items"]:
-            print("[Redeemed]", item["line_item_description"])
-    else:
-        # Error codes from https://steamstore-a.akamaihd.net/public/javascript/registerkey.js?v=qQS85n3B1_Bi&l=english
-        print('[ERROR CODE]', blob['purchase_result_details'])
+def ActivateKey(s_keys: str):
+    keys = re.findall('[a-zA-Z0-9]{5}-[a-zA-Z0-9]{5}-[a-zA-Z0-9]{5}', s_keys)
+    if not keys:
+        print("Not a valid key")
+        return
+    for a_key in keys:
+        r = user.session.post('https://store.steampowered.com/account/ajaxregisterkey/',
+                              data={
+                                  'product_key': a_key,
+                                  'sessionid': session["sessionid"]},
+                              headers={
+                                  'Cookie': f'sessionid={session["sessionid"]}; birthtime={session["birthtime"]}; lastagecheckage=1-0-1979; Steam_Language=russian; steamCountry={session["steamCountry"]}; steamLoginSecure={session["steamLoginSecure"]}'
+                              })
+        res = json.loads(r.text)
+        if res["success"] == 1:
+            for item in res["purchase_receipt_info"]["line_items"]:
+                print("[Redeemed]", item["line_item_description"])
+        else:
+            # Error codes from https://steamstore-a.akamaihd.net/public/javascript/registerkey.js?v=qQS85n3B1_Bi&l
+            print('[ERROR CODE]', res['purchase_result_details'])
 
 
 while True:
-    key = input('Enter key: ')
-    ActivateKey(key)
+    ActivateKey(input('Enter Steam key(s): '))
